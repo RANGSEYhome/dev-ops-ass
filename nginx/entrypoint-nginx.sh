6a0a4f45bb7085b7b679edd289bcf22b057e74e7
rangseyhome <info.rangsey@gmail.com>
2025-01-16 11:10:57 +0700

    Refactor Dockerfiles and docker-compose.yml for improved structure; switch to Yarn for dependency management, update build contexts, and implement a custom Nginx entrypoint script.

--- nginx/entrypoint-nginx.sh
+++ nginx/entrypoint-nginx.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-vars=$(compgen -A variable)
-subst=$(printf '${%s} ' $vars)
-envsubst "$subst" < /etc/nginx/conf.d/vhost.template > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+#!/bin/sh
+echo "Starting Nginx..."
+nginx -g "daemon off;"
